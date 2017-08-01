b93112ba71da81cc9e100dad31e5300cf264a5ae
lx <leixichina@126.com>
2017-08-01 22:08:12 +0800

    get web content over

--- GetWebContent.py
+++ GetWebContent.py
@@ -3,7 +3,8 @@ import urllib2
 from bs4 import BeautifulSoup
 import demjson as json
 
-url="http://bbs.tianya.cn/m/post-16-1667691-212.shtml"
+url="http://bbs.tianya.cn/m/post-16-1667691-2.shtml"
+#url="http://bbs.tianya.cn/m/post-16-1667691-1.shtml"
 htmlContent=urllib2.urlopen(url)
 soup = BeautifulSoup(htmlContent,"lxml")
 bbsGlobalDict = soup.script.contents[0].string.replace('var bbsGlobal = ','').replace(';','')
@@ -16,11 +17,22 @@ subType = user_dict['subType']
 
 print totalPage
 #print authorName
-#print title
-#print subType
-#print soup
 
-contentDIV = soup.find_all(name='div',attrs={"class":"reply-div"})
+floorClassName = 'item item-ht item-lz'
+floorContentDIVName = 'reply-div'
 
-for div in contentDIV:
-    print div.p
+floorIDName = 'data-id'
+floorUserName = 'data-user'
+
+floorDIV = soup.find_all(name='div',attrs={"class":floorClassName})
+#print floorDIV
+
+for floor in floorDIV:
+    dataID = floor.attrs[floorIDName]
+    dataUser = floor.attrs[floorUserName]
+    #print dataID
+    #print dataUser
+
+    floorContent = floor.find(name='div',attrs={"class":floorContentDIVName}).get_text()
+    print floorContent
+
