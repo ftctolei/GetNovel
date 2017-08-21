7e4f54534b36f7fed69ab11c9a6269cbf12184f5
lx <leixichina@126.com>
2017-08-21 17:08:55 +0800

    update getWebContent.py

--- GetWebContent.py
+++ GetWebContent.py
@@ -1,38 +1,73 @@
 #-*- coding: utf-8 -*-
+import logging
+import sys
 import urllib2
-from bs4 import BeautifulSoup
+
 import demjson as json
+from bs4 import BeautifulSoup
 
-url="http://bbs.tianya.cn/m/post-16-1667691-2.shtml"
-#url="http://bbs.tianya.cn/m/post-16-1667691-1.shtml"
-htmlContent=urllib2.urlopen(url)
-soup = BeautifulSoup(htmlContent,"lxml")
-bbsGlobalDict = soup.script.contents[0].string.replace('var bbsGlobal = ','').replace(';','')
-user_dict = json.decode(bbsGlobalDict)
+log_file = "./logs/GetNovel.log"
+#TODO 判断日志文件是否存在
 
-totalPage = user_dict['totalPage']
-authorName = user_dict['authorName']
-title = user_dict['title']
-subType = user_dict['subType']
+log_level = logging.DEBUG
+logger = logging.getLogger("loggingmodule.NomalLogger")
+handler = logging.FileHandler(log_file)
+formatter = logging.Formatter("[%(levelname)s][%(funcName)s][%(asctime)s]%(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(log_level)
 
-print totalPage
-#print authorName
 
-floorClassName = 'item item-ht item-lz'
-floorContentDIVName = 'reply-div'
+def getBsSoup(url):
+    try:
+        htmlContent = urllib2.urlopen(url)
+    except urllib2.HTTPError, e:
+        logger.error('get url content failed : ' + str(e.code))
+        sys.exit()
+    except urllib2.URLError, e:
+        logger.error('get url content failed : ' + str(e.reason))
+        sys.exit()
+    soup = BeautifulSoup(htmlContent, "lxml")
+    return soup
 
-floorIDName = 'data-id'
-floorUserName = 'data-user'
+#匹配天涯移动版 莲蓬鬼话 板块 帖子<<没有名字的人 >>
+#传入每页地址，返回回复是楼主的楼层内容
+def getTiantaPageContent(bsSoup ):
+    floorClassName = 'item item-ht item-lz'
+    floorContentDIVName = 'reply-div'
+    floorIDName = 'data-id'
+    floorUserName = 'data-user'
 
-floorDIV = soup.find_all(name='div',attrs={"class":floorClassName})
-#print floorDIV
+    floorDIV = bsSoup.find_all(name='div', attrs={"class": floorClassName})
+    resultContent = ''
+    dataID = ''
+    for floor in floorDIV:
+        #dataUser = floor.attrs[floorUserName]
+        dataID = floor.attrs[floorIDName]
+        floorContent = floor.find(name='div', attrs={"class": floorContentDIVName}).get_text()
+        resultContent = resultContent + floorContent
 
-for floor in floorDIV:
-    dataID = floor.attrs[floorIDName]
-    dataUser = floor.attrs[floorUserName]
-    #print dataID
-    #print dataUser
+    floorContentmap ={'floorID':dataID,'pageContent':resultContent}
+    return floorContentmap
 
-    floorContent = floor.find(name='div',attrs={"class":floorContentDIVName}).get_text()
-    print floorContent
+floorContentmap = {}
+def getPageTotal(bsSoup):
+    bbsGlobalDict = bsSoup.script.contents[0].string.replace('var bbsGlobal = ', '').replace(';', '')
+    user_dict = json.decode(bbsGlobalDict)
+    totalPage = user_dict['totalPage']
+    return totalPage
 
+
+
+url="http://bbs.tianya.cn/m/post-16-1667691-216.shtml"
+bsSoup = getBsSoup(url)
+pageTotal = getPageTotal(bsSoup)
+print pageTotal
+nowPage = 215
+#for page in range(nowPage,pageTotal+1):
+#    print page
+floorContentmap =  getTiantaPageContent(bsSoup)
+print floorContentmap['floorID']
+#print floorContentmap['pageContent']
+#print pageTotal
+# print len(pageContent)
